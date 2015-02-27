85c7bbb4c7b3a4f831f81e8d6f5a3e53054d2dbd
Charan Kumar <b.charan123@gmail.com>
2015-02-27 16:23:02 +0530

    Intial Commit

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,22 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController   
+  
+  def google_oauth2
+    if request.env["omniauth.auth"].info["email"] =~/[a-zA-Z_.1-9]*@crypsis.net/
+      @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+ 
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+
+    else
+      reset_session
+      redirect_to :root
+      flash[:error]= "Looks like you are out of crypsis network. Access is denied"
+    end
+
+  end
+end
