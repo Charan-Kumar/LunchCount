24d2baa0627feb07617e26ec34b6216a54849310
Charan Kumar <b.charan123@gmail.com>
2015-03-05 16:03:46 +0530

    Bug Fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, :except => [:index]
 
   def index
-
     if user_signed_in?
-        
-        redirect_to :controller=>'home', :action => 'dashboard'
+      redirect_to :controller=>'home', :action => 'dashboard'
     
     end
   
