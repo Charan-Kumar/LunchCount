3b70d0f1a864f78d2b94dcb757b21e5ca706008f
Charan Kumar <b.charan123@gmail.com>
2015-03-09 18:34:26 +0530

    Adds Admin Count Page

--- app/models/food_count.rb
+++ app/models/food_count.rb
@@ -10,7 +10,7 @@ class FoodCount < ActiveRecord::Base
     bom = dt.beginning_of_month
     eom = dt.end_of_month
     sel = "Yes"
-    where("date >= ? and date <= ? and selection == ?", bom, eom,sel).group(:date).count
+    where("date >= ? and date <= ? and selection = ?", bom, eom,sel).group(:date).count
   end
 
   # def self.user_current_month
