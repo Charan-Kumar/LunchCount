b5ca25875b9cab6616124448d19ea17bd0d066a1
Charan Kumar <b.charan123@gmail.com>
2015-03-17 14:00:06 +0530

    Bug in Count Query

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,16 +16,28 @@ class AdminController < ApplicationController
       h=params[:from]
       from = Date.new(h['Starting Day(1i)'].to_i, h['Starting Day(2i)'].to_i, h['Starting Day(3i)'].to_i).to_s
       h=params[:to]
-      to = Date.new(h['Ending Day(1i)'].to_i, h['Ending Day(2i)'].to_i, h['Ending Day(3i)'].to_i).to_s
+      month = h['Ending Day(2i)'].to_i
+      if(h['Ending Day(3i)'].to_i == 31 and (month == 4 or month == 6 or month == 9 or month ==11))
+        to = Date.new(h['Ending Day(1i)'].to_i, h['Ending Day(2i)'].to_i, 30).to_s
+
+      elsif(h['Ending Day(2i)'].to_i == 2)
+        to = Date.new(h['Ending Day(1i)'].to_i, h['Ending Day(2i)'].to_i, 28).to_s
+      else
+        to = Date.new(h['Ending Day(1i)'].to_i, h['Ending Day(2i)'].to_i, h['Ending Day(3i)'].to_i).to_s
+      end
+
       @count = FoodCount.date_range(from,to)
     end
   end
 
   def future_count
-    @fc=FoodCount.every_month(params[:date])
-    respond_to do |format|
-      format.html # this handles normal requests asking for html
-      format.json{ render json: @fc }
-    end
+    # if params[:date]
+    #    @fc=FoodCount.every_month(params[:date])
+    # end
+   
+    # respond_to do |format|
+    #   format.html # this handles normal requests asking for html
+    #   format.json{ render json: @fc }
+    # end
   end
 end
